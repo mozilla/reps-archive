dbee47ba584b03053747a0f6aa3fbe5b295cdfa2
Michael Kohler <me@michaelkohler.info>
2020-06-26 18:43:36 +0200

    More tests + fix Alumni full name spacing

--- tests/lib/fetch.test.js
+++ tests/lib/fetch.test.js
@@ -2,10 +2,12 @@ import test from 'ava';
 import nock from 'nock';
 import sinon from 'sinon';
 import fs from 'fs';
+import path from 'path';
 import fetch from '../../lib/fetch';
 
 const BASE_URL = 'https://reps.mozilla.org';
-const REPS_ENDPOINT = `/api/remo/v1/users`;
+const REPS_ENDPOINT = '/api/remo/v1/users';
+const ALUMNI_ENDPOINT = '/people/alumni';
 
 test.beforeEach((t) => {
   t.context.sandbox = sinon.createSandbox();
@@ -57,9 +59,17 @@ test.serial('should fetch and save data', async (t) => {
       name: 'Baz',
     });
 
+  const ALUMNI_FIXTURE = await fs.promises.readFile(path.join(__dirname, '..', 'fixtures', 'alumni.html'), 'utf8');
+  new Array(26).fill(0)
+    .forEach((_, index) => {
+      nock(BASE_URL)
+        .get(`${ALUMNI_ENDPOINT}/?page=${index + 1}`)
+        .reply(200, ALUMNI_FIXTURE);
+    });
+
   await fetch.fetchAllData([{
     url: `${BASE_URL}${REPS_ENDPOINT}`,
-    name: 'REPS_TEST',
+    name: 'REPS',
   }]);
 
   const expected = [
@@ -76,7 +86,19 @@ test.serial('should fetch and save data', async (t) => {
 
   t.true(fs.promises.writeFile.called);
   const [, writeArgument] = fs.promises.writeFile.getCall(0).args;
-  t.is(writeArgument, JSON.stringify(expected, null, 2));
+  const reps = JSON.parse(writeArgument);
+  t.is(reps.length, 3 + (20 * 26));
+  t.deepEqual(reps[0], expected[0]);
+  t.deepEqual(reps[1], expected[1]);
+  t.deepEqual(reps[2], expected[2]);
+  t.deepEqual(reps[3], {
+    alumni: true,
+    full_name: 'Pierros Papadeas', // eslint-disable-line camelcase
+    display_name: 'pierros', // eslint-disable-line camelcase
+    date_joined_program: '2011-06-11T00:00:00', // eslint-disable-line camelcase
+    date_left_program: '2017-06-06T00:00:00', // eslint-disable-line camelcase
+    country: 'Greece',
+  });
 });
 
 test.serial('should throw if fetch unsuccessful', async (t) => {
