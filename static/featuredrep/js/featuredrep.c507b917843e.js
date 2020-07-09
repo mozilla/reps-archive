1efaa98dc18868d1d440680104a7459f08991c1c
Irvin <irvinfly@gmail.com>
2020-07-10 02:09:44 +0800

    merge static-archive into gh-pages (#3)
    * add pages archive by SiteSucker on 20200408
    
    * add list of reps to /people/index.html
    
    * add some more events and reps
    
    * add some more events and reps
    
    * add events missing from last archive
    
    * add u/viralparmar
    
    * move yofie to alumni
    
    * add sample events from 2012/5/1-2012/7/31
    
    * udpate u/yofie and kpi
    
    * update planet on /
    
    * add event lists from 2012-2020

---
+++ static/featuredrep/js/featuredrep.c507b917843e.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('a.featured-delete').click(function (e) {
+        e.preventDefault();
+        $(this).closest('form').submit();
+    });
+    paginatorSelector();
+});
+
